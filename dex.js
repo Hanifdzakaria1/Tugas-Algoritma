6657d526af3bee948516a8754f7cf5ceeab0bd70
Hanifdzakaria1 <118792497+Hanifdzakaria1@users.noreply.github.com>
2023-08-02 12:28:12 +0700

    Update dex.js

--- dex.js
+++ dex.js
@@ -3,12 +3,9 @@ let jam = [2,2,3,3,1,4,5];
 
 let totjamlebih = 0
 let totjamkurang = 0
-let totharilebih =0
 for (let i=0; i< jam.length; i++) {
     totjamlebih += jam[i]
     if (jam[i]> 2 ) {
-        totjamlebih++
-    } else if (jam[i] < 3) {
         totjamkurang++
     } 
 }
